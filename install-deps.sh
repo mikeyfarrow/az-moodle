6511af59bd0d51069beb34648e42b299f6bbd8e1
Mikey Farrow <mikey@un-loop.org>
2017-07-09 00:48:25 -0700

    do not install moodle yet

--- install-deps.sh
+++ install-deps.sh
@@ -12,9 +12,9 @@ easy_install supervisor
 ./foreground.sh /etc/apache2/foreground.sh
 ./supervisord.conf /etc/supervisord.conf
 
-curl https://download.moodle.org/moodle/moodle-latest.tgz > /var/www/moodle-latest.tgz
-cd /var/www; tar zxvf moodle-latest.tgz; mv /var/www/moodle /var/www/html
-chown -R www-data:www-data /var/www/html/moodle
-mkdir /var/moodledata
-chown -R www-data:www-data /var/moodledata; chmod 777 /var/moodledata
-chmod 755 /start.sh /etc/apache2/foreground.sh
+# curl https://download.moodle.org/moodle/moodle-latest.tgz > /var/www/moodle-latest.tgz
+# cd /var/www; tar zxvf moodle-latest.tgz; mv /var/www/moodle /var/www/html
+# chown -R www-data:www-data /var/www/html/moodle
+# mkdir /var/moodledata
+# chown -R www-data:www-data /var/moodledata; chmod 777 /var/moodledata
+# chmod 755 /start.sh /etc/apache2/foreground.sh
